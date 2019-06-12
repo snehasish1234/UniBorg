d43f077fe9010068c2669b7074823fc96fcb6e59
snehasish1234 <51477672+snehasish1234@users.noreply.github.com>
2019-06-12 22:14:53 +0530

    Update 5_6055200290112536710.py

--- stdplugins/5_6055200290112536710.py
+++ stdplugins/5_6055200290112536710.py
@@ -45,7 +45,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f" ᴠɪʀᴜᴢZ 🔥⌚{HM}🔥📅{DM}"
+        name = f"🔥⌚{HM}Mr Joker🔥📅{DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
